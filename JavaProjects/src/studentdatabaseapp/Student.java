48c5366bb518d1341c9dd987330d0b6e815a5e24
Jessie <Jessie@10.0.0.3>
2021-03-16 21:25:41 -0400

    added enrollStudent function

--- JavaProjects/src/studentdatabaseapp/Student.java
+++ JavaProjects/src/studentdatabaseapp/Student.java
@@ -6,19 +6,19 @@ import java.util.Scanner;
 public class Student {
 
 private String userName;
-private int numberOfStudents;
 private int studentYear;
 private String studentNumber;
+private int answer;
 Scanner in = new Scanner (System.in);
 
-//////////////////Constructor to get number of students 
+//////////////////Constructor////////////////////////////////
 public Student (){
 	String userName = this.userName;
 	int studentYear = this.studentYear;
 	String studentNumber = this.studentNumber;
+	int answer = this.answer;
 	
-	//call a method to get the number of students 
-	this.numberOfStudents = getNumberOfStudents();
+	getNumberOfStudents();
 	
 	//Call a method to ask user the name of student
 	this.userName = getUserName();
@@ -30,15 +30,24 @@ public Student (){
 	//Call method to display  user information 
 	displayStudentInfo(userName, studentYear, studentNumber);
 	
+	//Ask user if they would like to enroll a student
+	System.out.println("\nWhat would you like to do?\n ");
+	System.out.println("Enrollment [1]" + "\nDrop Course [2]" + "\nSee Student Bill [3]");
+	in = new Scanner(System.in);
+	answer = in.nextInt();
+	
+	if (answer == 1) 
+		enrollStudent();
+	
+	
+	
 }
-/////////////////End Constructor 
+/////////////////End Constructor//////////////////////////////////////////////// 
 
 //Get the number of students being entered
 public int getNumberOfStudents()  {
 	System.out.println("Enter number of Students: ");
-	 int numStudents = in.nextInt();
-	//int numStudents = in;
-	//in.close();															//Close scanner to prevent memory leak
+	 int numStudents = in.nextInt();													//Close scanner to prevent memory leak
 	return numStudents; 
 	
 }
@@ -47,7 +56,7 @@ public int getNumberOfStudents()  {
 public String getUserName(){
 	in = new Scanner (System.in);
 
-	System.out.println("Enter Student Name: ");
+	System.out.println("Enter Student First and Last Name");
 	userName = in.nextLine();
 
     return userName;    
@@ -70,23 +79,25 @@ public String getStudentNumber(){
   System.out.println("Enter the year of the student: "); in = new
   Scanner(System.in); 
    studentYear = in.nextInt();
-  //studentYear.toString();
-  in.close(); //Close
   return studentYear;
  }
  
  //Display student info 
  public void displayStudentInfo(String userName, int studentYear, String studentNumber) {
-	String userName1 = this.userName;
+	String userName_2 = this.userName;
 	int stuYear = this.studentYear;
 	String stuNum = this.studentNumber;
 	
 	System.out.println("Name" + "              " +  "Year" +"            " + "Student Number");
-	System.out.println(userName1 + "           " + stuYear + "               " + stuNum);
+	System.out.println(userName_2 + "           " + stuYear + "               " + stuNum);
 	 
 	 
 	 return;
- }
+ 	}
  
-
+ //Enroll Student in course 
+ public void enrollStudent() {
+	 System.out.println("Select course below");
+	 
+ }
 }
